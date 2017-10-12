e480ca69a53d989c9cfa77f8a33e485a1e5b691b
axawzh <wuevans@hotmail.com>
2017-10-13 02:07:07 +0800

    some changes

---
+++ app/src/androidTest/java/com/klipspringercui/sgbusgo/MainActiviryText.java
@@ -0,0 +1,24 @@
+package com.klipspringercui.sgbusgo;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by Zhenghao on 3/4/17.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class MainActiviryText {
+    @Rule
+    public ActivityTestRule<MyProfileActivity> mActivityRule =
+            new ActivityTestRule<MyProfileActivity>(MyProfileActivity.class);
+
+    @Test
+    public void testAddFrequentTrip() {
+
+    }
+}
